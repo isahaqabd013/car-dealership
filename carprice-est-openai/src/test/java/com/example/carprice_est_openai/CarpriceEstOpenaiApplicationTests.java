dbd30ddbec9d0859e0c6d36378948e845ae054be
isahaqabd013 <isahaqabd@gmail.com>
2025-01-21 11:50:55 +0530

    First Commit

---
+++ carprice-est-openai/src/test/java/com/example/carprice_est_openai/CarpriceEstOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carprice_est_openai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpriceEstOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
